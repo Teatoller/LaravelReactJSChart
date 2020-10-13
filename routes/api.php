7438aad00600964b4db88620b0abb4156e7b787b
Teatoller <steven.otieno1@gmail.com>
2020-10-13 20:23:27 +0300

    (chart data): Implement chart data
    - create model
    - design schema
    - connect database
    - run migrations
    - add controller
    - add routes
    
    [Starts #001]

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ChartController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:api')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+// Route::apiResources([
+//     'register' => AuthController::class,
+// ]);
+// Route::get('register', [AuthController::class, 'register']);
+
+Route::get('charts', [ChartController::class, 'index']);
+
